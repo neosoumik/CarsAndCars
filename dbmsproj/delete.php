8005c6268bcba78ffbcd6a6aa0ecb4515b668108
Soumith K <42935232+neosoumik@users.noreply.github.com>
2020-07-21 18:06:14 +0530

    Init push

---
+++ dbmsproj/delete.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+$servername = "localhost";
+$username = "hks";
+$password = "donut";
+$dbname = "cars";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to delete a record
+$sql = "DELETE FROM cars WHERE id=0";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
